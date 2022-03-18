daf4c6166b31e5a123302325ea8250f7ac1dbd39
TheApplePieGod <fishplaysgame@gmail.com>
2022-03-17 22:03:17 -0500

    Finish about page refactor

---
+++ src/Components/UI/SkillBar.tsx
@@ -0,0 +1,58 @@
+﻿import * as React from "react";
+import { styled, Box, LinearProgress, Typography, Grid } from "@mui/material";
+import Image from "next/image";
+
+interface Props {
+    percentage: number;
+    skillName: string;
+    yearCount: number;
+    iconPath: string;
+    iconAlt: string;
+}
+
+export const SkillBar: React.FunctionComponent<Props> = (props) => {
+    return (
+        <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Box
+                sx={{
+                    minWidth: { xs: 35, sm: 45, md: 55, lg: 65 },
+                    minHeight: { xs: 35, sm: 45, md: 55, lg: 65 },
+                    position: "relative",
+                    marginRight: "0.5rem"
+                }}
+            >
+                <Image src={props.iconPath} alt={props.iconAlt} layout="fill" />
+            </Box>
+            <Box sx={{ width: "100%" }}>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <Typography variant="h4" color="text.primaryDark">{props.skillName}</Typography>
+                    <Box sx={{ display: "flex", gap: { xs: "0.75rem", sm: "1rem" }, alignItems: "center" }}>
+                        <Typography variant="body1" color="text.secondaryDark">{`${props.yearCount} yr${props.yearCount != 1 ? 's' : ''}`}</Typography>
+                        <Typography variant="h4" color="text.primaryDark">{`${props.percentage}%`}</Typography>
+                    </Box>
+                </Box>
+                <LinearProgress
+                    variant="determinate"
+                    value={props.percentage}
+                    sx={{
+                        height: "2vw",
+                        borderRadius: "1vw",
+                        backgroundColor: "text.secondary",
+                        "& .MuiLinearProgress-bar": {
+                            borderRadius: "5px"
+                        }
+                    }}
+                />
+            </Box>
+        </Grid>
+    );
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 ﻿import * as React from "react";
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import { NextSeo } from "next-seo";
 import { Section } from "../Components/UI/Section";
 import { useAppSelector } from "../Redux/Hooks";
@@ -10,6 +10,7 @@ import SvgIcon from "@mui/material/SvgIcon";
 import ComputerIcon from '@mui/icons-material/Computer';
 import CodeIcon from '@mui/icons-material/Code';
 import CategoryIcon from '@mui/icons-material/Category';
+import { SkillBar } from "../Components/UI/SkillBar";
 
 const InterestBlock = (props: { text: string; icon: typeof SvgIcon }) => {
     return (
@@ -117,6 +118,22 @@ const AboutPage = () => {
                                 />
                             </Box>
                         </StarrySection>
+                        <Typography variant="h3" color="text.primaryDark" sx={{ marginTop: "60px" }}>Skills</Typography>
+                        <CircleLine count={5} />
+                        <Typography variant="h5" color="text.secondaryDark" sx={{ marginBottom: "40px" }}>Confidence</Typography>
+                        <Grid container spacing={{ xs: 2, sm: 3, md: 4 }} columnSpacing={6} columns={12} sx={{ padding: { xs: "0 30px", md: "0 60px" }, maxWidth: "1400px" }}>
+                            <SkillBar percentage={90} yearCount={4} skillName="ReactJS" iconPath="/images/react.png" iconAlt="React Logo" /> {/* 2019 */}
+                            <SkillBar percentage={90} yearCount={8} skillName="HTML/CSS" iconPath="/images/csshtml.svg" iconAlt="HTML/CSS Logo" /> {/* 2015 */}
+                            <SkillBar percentage={90} yearCount={7} skillName="JS/TS" iconPath="/images/typescript.png" iconAlt="JS/TS Logo" /> {/* 2016 */}
+                            <SkillBar percentage={90} yearCount={6} skillName="C++" iconPath="/images/cpp.svg" iconAlt="C++ Logo" /> {/* 2017 */}
+                            <SkillBar percentage={80} yearCount={5} skillName="C#/DotNet" iconPath="/images/cs.svg" iconAlt="C#/DotNet Logo" /> {/* 2018 */}
+                            <SkillBar percentage={80} yearCount={4} skillName="SQL" iconPath="/images/sql.png" iconAlt="SQL Logo" /> {/* 2019 */}
+                            <SkillBar percentage={70} yearCount={3} skillName="Python" iconPath="/images/python.png" iconAlt="Python Logo" /> {/* 2020 */}
+                            <SkillBar percentage={50} yearCount={3} skillName="Azure" iconPath="/images/azure.png" iconAlt="Azure Logo" /> {/* 2020 */}
+                        </Grid>
+                        
+                        {/* Spacer */}
+                        <Box sx={{ height: "150px" }}/>
                     </Box>
                 }
             </Box>
