2f7be347b7ef83eb76016adeca61f76379d19956
TheApplePieGod <evant.002@gmail.com>
2023-03-27 23:44:09 -0500

    Animated background

--- src/Components/UI/StarrySection.tsx
+++ src/Components/UI/StarrySection.tsx
@@ -16,13 +16,13 @@ const StyledSectionStarry = styled(StyledSection)(
         width: 100vw;
         margin: 10px 0 10px 0;
         clip-path: polygon(0 50px, 100% 0, 100% calc(100% - 50px), 0 100%);
-        background-image: url(images/background-nomoon.jpg);
+        background-image: url(images/background-animated.webp);
         padding-bottom: 75px;
         padding-top: 75px;
-        ${theme.breakpoints.up('xs')} {
+        ${theme.breakpoints.up("xs")} {
             background-position: center;
         }
-        ${theme.breakpoints.up('md')} {
+        ${theme.breakpoints.up("md")} {
             background-attachment: fixed;
             background-position: left;
             background-size: 100vw;
@@ -40,27 +40,30 @@ export const StarrySection: React.FunctionComponent<Props> = (props) => {
     const [scrollY, setScrollY] = React.useState(0);
 
     const onScroll = () => {
-        const winScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        const winScroll =
+            window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop ||
+            0;
         setScrollY(winScroll);
-    }
+    };
 
     React.useEffect(() => {
         if (parallax && !props.disabled) {
             window.addEventListener("scroll", onScroll);
             return () => {
                 window.removeEventListener("scroll", onScroll);
-            }
+            };
         }
     }, [parallax, props.disabled]);
 
-    return (
-        props.disabled ?
-        <StyledSection>
-            {props.children}
-        </StyledSection>
-        :
-        <StyledSectionStarry style={{ backgroundPositionY: parallax ? -scrollY * 0.25 : 0 }}>
+    return props.disabled ? (
+        <StyledSection>{props.children}</StyledSection>
+    ) : (
+        <StyledSectionStarry
+            style={{ backgroundPositionY: parallax ? -scrollY * 0.25 : 0 }}
+        >
             {props.children}
         </StyledSectionStarry>
     );
-}
+};
